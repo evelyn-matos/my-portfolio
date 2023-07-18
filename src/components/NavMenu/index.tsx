98a0501c9dc3efdf2c420f678a5b701e5e4f529c
Evelyn Matos <matos.evelyn1.8@gmail.com>
2023-07-18 16:54:47 -0300

    fix: corrigindo menu de navegacao, melhorando usabilidade e responsividade

--- src/components/NavMenu/index.tsx
+++ src/components/NavMenu/index.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import './styles.css';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import Tooltip from '@mui/material/Tooltip';
 import { Icones } from '@/shared/iconsNav';
 import {useRouter, usePathname} from 'next/navigation'
 
@@ -25,14 +26,23 @@ export default function MenuNav() {
    
 
     return (
-        <nav className='flex w-full h-[90px] p-5 fixed left-0 bottom-0 bg-background-ligth dark:bg-background-dark md:bg-transparent md:absolute md:top-0 md:w-[500px] md:ml-[30%] md:items-center md:justify-center  '>
-            <BottomNavigation value={path} onChange={handleChange}  className='p-2 bg-background-ligth dark:bg-background-dark ' >
+        <nav className='flex flex-col items-center  xl:justify-center  right-0  fixed h-max bottom-0 mt-auto  xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen '>
+            <BottomNavigation 
+                value={path} 
+                onChange={handleChange}  
+                className='flex w-full   bg-background-dark/10  backdrop-blur-sm xl:flex-col items-center  xl:justify-center  px-0 md:px-40 xl:px-0 h-[50px] xl:h-[400px] py-8  md:text-3xl xl:text-xl xl:rounded-full xl:border xl:border-gray' >
                 
                 <BottomNavigationAction
-                    label="Home"
+                    className='  text-gray hover:text-pink transition duration-700 ease-in-out '
+                    label='Home'
                     value="/"
-                    icon={<Icones.HomeIcon />}
-                    className='active:text-pink'
+                    icon={
+                        <Tooltip title='Home'>
+                            <Icones.HomeIcon className='w-[20px]'  />
+                        </Tooltip>
+                    
+                    }
+                    
                     
                        
                  />
@@ -40,33 +50,63 @@ export default function MenuNav() {
                
 
                 <BottomNavigationAction
-                    label="Sobre"
+                    className='w-[30px] p-0 m-0 ml-[-30px] xl:ml-0  text-gray hover:text-pink transition duration-700 ease-in-out'
+                    label='Sobre'
                     value="/sobre"
-                    icon={<Icones.PersonIcon />}
+                    icon={
+                        <Tooltip title='Sobre'>
+                            <Icones.PersonIcon className='w-[20px] p-0 m-0' />
+                        </Tooltip>
+                    }
+                    
                 />
 
                 <BottomNavigationAction
-                    label="Estudos"
+                    className='w-[30px] p-0 m-0 ml-[-30px] xl:ml-0  text-gray hover:text-pink transition duration-700 ease-in-out'
+                    label='Estudos'
                     value="/estudos"
-                    icon={<Icones.AutoStoriesIcon />}
+                    icon={
+                        <Tooltip title='Estudos'>
+                            <Icones.AutoStoriesIcon className='w-[20px] p-0 m-0' />
+                        </Tooltip>
+                }
+                    
                 />
 
                 <BottomNavigationAction 
-                    label="Skills" 
+                    className='w-[30px] p-0 m-0 ml-[-30px] xl:ml-0  text-gray hover:text-pink transition duration-700 ease-in-out'
+                    label='Skills'
                     value="/skills" 
-                    icon={<Icones.PsychologyIcon />} 
+                    icon={
+                        <Tooltip title='Skills'>
+                            <Icones.PsychologyIcon className='w-[20px] p-0 m-0'/>
+                        </Tooltip>
+                    } 
+                    
                 />
 
                 <BottomNavigationAction 
-                    label="Projetos" 
+                    className='w-[30px] p-0 m-0 ml-[-30px] xl:ml-0  text-gray hover:text-pink transition duration-700 ease-in-out'
+                    label='Projetos'
                     value="/projetos" 
-                    icon={<Icones.SplitscreenIcon />} 
+                    icon={
+                        <Tooltip title='Projetos'>
+                            <Icones.SplitscreenIcon className='w-[20px] p-0 m-0'/>
+                        </Tooltip>
+                    } 
+                    
                 />
 
                 <BottomNavigationAction 
-                    label="Contato" 
+                    className='w-[30px] p-0 m-0 ml-[-30px] xl:ml-0  text-gray hover:text-pink transition duration-700 ease-in-out' 
+                    label='Contato'
                     value="/contato" 
-                    icon={<Icones.EmailIcon />} 
+                    icon={
+                        <Tooltip title='Contato'>
+                            <Icones.EmailIcon className='w-[20px] p-0 m-0'/>
+                        </Tooltip>
+                    } 
+                    
                 />
 
         </BottomNavigation>
