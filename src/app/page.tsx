df8623126152b0665d18e6b86d265b1362c47e5e
Evelyn Matos <matos.evelyn1.8@gmail.com>
2023-07-18 16:55:38 -0300

    fix: alterando tamanho da area main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,13 +14,13 @@ import photoDev from '../../public/images/imagedev.jpeg';
 
 export default function Home() {
   return (
-    <main >
+    <main className='w-full md:max-w-[1150px] ' >
       <h1 className="block text-3xl mb-3 md:text-5xl leading-10 md:mb-6 text-black dark:text-white ">Hello Recruiter!<br/> Meu nome é <span className="font-indie  text-3xl md:text-5xl text-pink">Evelyn</span>, sou desenvolvedora Frontend</h1>
       <p className="text-base text-black dark:text-white">Aqui você descobrirá mais sobre mim e meus projetos!</p>
       <p className="text-base text-black dark:text-white" >Fique a vontade para entrar em contato comigo, em caso de qualquer dúvida</p>
       <div className='md:flex '>
         
-        <section className='mt-8 flex  items-center md:flex-1'>
+        <section className='mt-8 flex flex-col items-center md:flex-row md:flex-1'>
           <div className='w-52 h-52 '>
             <Image 
               src={photoDev}
@@ -36,20 +36,20 @@ export default function Home() {
           </div>
           
           <div className='w-52 h-28 flex flex-col items-center justify-between '>
-            <div>
+            <div className='mt-5'>
               <IconButton href='linkedin.com'  >
                 <Tooltip title='Linkdin'>
-                  <LinkedInIcon className='hover:text-pink transition duration-700 ease-in-out'/>
+                  <LinkedInIcon className='text-gray hover:text-pink transition duration-700 ease-in-out'/>
                 </Tooltip>
               </IconButton>
               <IconButton href='github.com'>
                 <Tooltip title='Github'>
-                  <GitHubIcon className='hover:text-pink transition duration-700 ease-in-out'/>
+                  <GitHubIcon className='text-gray hover:text-pink transition duration-700 ease-in-out'/>
                 </Tooltip>
               </IconButton>
               <IconButton href='instagram.com'>
                 <Tooltip title='Instagram'>
-                  <InstagramIcon className='hover:text-pink transition duration-700 ease-in-out'/>
+                  <InstagramIcon className='text-gray hover:text-pink transition duration-700 ease-in-out'/>
                 </Tooltip>
               </IconButton>
             </div>
@@ -59,7 +59,7 @@ export default function Home() {
           </div>
         </section>
         <section className=' mt-8 mb-20 md:flex-1 '>
-          <span className="block font-indie text-lg  text-center text-pink md:text-3xl md:mt-12">Principais Tecnologias</span>
+          <span className="block font-indie text-xl  text-center text-pink md:text-3xl md:mt-12">Principais Tecnologias</span>
           <ul className='flex justify-evenly mt-10 '>
             <li className="w-12 h-12 ">
               <Tooltip title='TypeScript'>
